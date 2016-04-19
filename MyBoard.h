91e0382379e3d5072597f1edf16bf6adfdc60e17
YHB55 <YHB55@users.noreply.github.com>
2016-04-19 06:41:35 +0100

    Added Files

---
+++ MyBoard.h
@@ -0,0 +1,62 @@
+// MyBoard -- hardware MyBoard definitions for YHB
+// Pins for Sensors, LCD, Ouputs etc.
+// Make sure you use the exact same names as use in the main sketch
+
+// Sensors
+// Data wire is plugged into pin A3 on the Arduino
+#define ONE_WIRE_BUS 57
+
+// Rotary switch pins
+#define FIRST_ROTARY_PIN 58
+#define LAST_ROTARY_PIN 69
+
+// Ouputs
+#define PUMP_PIN 6	//pin for relay output (pump)
+#define BUZZER_PIN 8    //pin for buzzer output
+#define ELEMENT_PIN 9   //pin for SSR output (heat)
+
+// Summary Of Available Pins
+//pin 54 - Analogue pin A0 - Not Used
+//pin 55 - Analogue pin A1 - Not Used
+//pin 56 - Shortcut Switch - Analogue pin A2
+//pin 57 - One Wire Bus - Analogue pin A3
+//pin 58 Mode 1 - Analogue pin A4
+//pin 59 Mode 2 - Analogue pin A5
+//pin 60 Mode 3 - Analogue pin A6
+//pin 61 Mode 4 - Analogue pin A7
+//pin 62 Mode 5 - Analogue pin A8
+//pin 63 Mode 6 - Analogue pin A9
+//pin 64 Mode 7 - Analogue pin A10
+//pin 65 Mode 8 - Analogue pin A11
+//pin 66 Mode 9 - Analogue pin A12
+//pin 67 Mode 10 - Analogue pin A13
+//pin 68 Mode 11 - Analogue pin A14
+//pin 69 Mode 12 - Analogue pin A15
+
+//pin rx 0 USED FOR SCREEN
+//pin tx 1 USED FOR SCREEN
+//pin PWM 2 USED FOR SCREEN
+//pin PWM 3 USED FOR SCREEN
+//pin PWM 4 USED FOR SCREEN
+//pin PWM 5 USED FOR SCREEN
+//pin PWM 6 USED FOR SCREEN
+//pin PWM 7 USED FOR SCREEN
+
+//pin PWM 8 rx - Not Used
+//pin PWM 9 rx - Not Used
+//pin PWM 10 - Not Used
+//pin PWM 11 used for tone ac
+//pin PWM 12 used for tone ac
+//pin PWM 13 - Not Used
+//pin GND - Not Used
+//pin AREF - Not Used
+
+//pin 14 TX3 - Not Used
+//pin 15 RX3 - Not Used
+//pin 16 TX2 - Not Used
+//pin 17 RX2 - Not Used
+//pin 18 TX1 - Not Used
+//pin 19 RX1 - Not Used
+//pin 20 SDA - Not Used
+//pin 21 SCL - Not Used
+
